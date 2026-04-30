0c5100589d1dfe75d702b08c515683003be53837
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-29 20:48:44 -0700

    Added the SoccerTeamContext

---
+++ COMP003B.Assignment6/Data/SoccerTeamContext.cs
@@ -0,0 +1,16 @@
+﻿using COMP003B.Assignment6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.Assignment6.Data
+{
+	public class SoccerTeamContext : DbContext
+	{
+		public SoccerTeamContext(DbContextOptions<SoccerTeamContext> options) : base(options) { }
+
+		
+		//
+		public DbSet<Player> Players { get; set; }
+		public DbSet<Team> Teams { get; set; }
+		public DbSet<PlayerTeam> PlayerTeams { get; set; }
+	}
+}

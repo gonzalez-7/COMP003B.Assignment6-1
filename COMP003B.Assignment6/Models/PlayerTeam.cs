6b771e7811f5e25ef38bd5304ca0b9d3a4f22080
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-29 20:20:36 -0700

    Added Model for Team and Players Team

---
+++ COMP003B.Assignment6/Models/PlayerTeam.cs
@@ -0,0 +1,13 @@
+﻿namespace COMP003B.Assignment6.Models
+{
+	public class PlayerTeam
+	{
+		public int Id { get; set; }
+		public int PlayerId { get; set; }
+		public int TeamId { get; set; }
+
+		public virtual Player? Player { get; set; }
+
+		public virtual Team? Team { get; set; }
+	}
+}
---
+++ COMP003B.Assignment6/Models/Team.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+	public class Team
+	{
+		public int TeamId { get; set; }
+
+		[Required]
+		public string TeamName { get; set; } = string.Empty;
+
+		[Required]
+		public string City { get; set; } = string.Empty;
+
+		public virtual ICollection<PlayerTeam>? PlayerTeams { get; set; }
+	}
+}

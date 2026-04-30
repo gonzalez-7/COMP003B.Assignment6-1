d264b381101fac7ec720f1cbfc367ff0fbaec91f
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-29 21:11:40 -0700

    removed unnecessary things

--- COMP003B.Assignment6/Models/Player.cs
+++ COMP003B.Assignment6/Models/Player.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace COMP003B.Assignment6.Models
 {

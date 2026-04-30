b614afef673892b8db63f4d48721d44c30ce75d3
gonzalez-7 <20agonzalezp@gmail.com>
2026-04-29 20:08:45 -0700

    Added Model for Players

---
+++ COMP003B.Assignment6/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment6.Models
+{
+	public class Player
+	{
+		public int PlayerId { get; set; }
+
+		[Required]
+		public string Name { get; set; } = string.Empty;
+
+		[Required]
+		public string Position {  get; set; } = string.Empty;
+
+		public int JerseyNumber { get; set; }
+
+		public virtual ICollection<PlayerTeam>? PlayerTeams { get; set; }
+	}
+}
